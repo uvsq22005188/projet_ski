16e926c456ac6c2f4c2803d0df2d70ff0a1a3f9d
Kaan DOYURUR <kaan.doyurur@ens.uvsq.fr>
2023-04-18 00:04:00 +0200

    creation point + save + load

--- interface.py
+++ interface.py
@@ -1,31 +1,23 @@
 import tkinter as tk
+import pickle
 from tkinter import Scrollbar
-
-
-class Sommet():
-    def __init__(self):
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
-
+from tkinter.filedialog import asksaveasfile, askopenfile
+from graphe import Noeud, Graphe
 
 
 class MainApplication(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
+    def __init__(self, root=None):
+        super().__init__(root)
+        self.root = root
         self.pack()
         self.create_widgets()
+
+        self.taille = 15
+        self.debut_x, self.debut_y = None, None
+        self.ligne = None
+        self.current = None
+        self.retour_liste = []
+        self.noeud1, self.noeud2 = None, None
 
     def create_widgets(self):
         # Création d'un Canvas
@@ -33,6 +25,15 @@ class MainApplication(tk.Frame):
         self.height = 720
         self.canvas = tk.Canvas(
             self, width=self.width, height=self.height, scrollregion=(0, 0, 3600, 3000))
+
+        # Creation bouttons
+
+        self.b_sauvegarder = tk.Button(
+            root, text="Sauvegarder Points", command=self.sauver_points)
+        self.b_charger = tk.Button(
+            root, text="Charger Points", command=self.charger_points)
+        self.b_sauvegarder.pack()
+        self.b_charger.pack()
 
         # Creation scrollbar x
 
@@ -55,33 +56,118 @@ class MainApplication(tk.Frame):
         self.canvas.create_image(
             1800, 1500, image=self.background_img, anchor="center")
 
-        # Ajout d'un bouton pour placer des points d'intersection
-        self.button = tk.Button(
-            self, text="Placer un point", command=self.place_intersection_point)
-        self.button.pack(side="left")
-
         # Ajout d'une variable pour stocker les points d'intersection
-        self.intersection_points = []
+        self.liste_noeuds = []
 
         # Bind du clic gauche sur le canvas pour ajouter des points d'intersection
-        self.canvas.bind("<Button-1>", self.add_intersection_point)
+        self.canvas.bind("<Button-1>", self.ajouter_noeud)
+        self.canvas.bind("<ButtonPress-3>", self.debut_ligne)
+        self.canvas.bind("<ButtonRelease-3>", self.tracer_ligne)
+        self.root.bind("<Control-z>", self.retour_arriere)
 
-    def place_intersection_point(self):
-        # Permet à l'utilisateur de cliquer sur le canvas pour placer un point
-        self.canvas.config(cursor="plus")
+    def sauver_points(self):
+        files = [('Text Document', '*.pkl')]
+        file = asksaveasfile(
+            filetypes=files, defaultextension=files, mode="wb")
+        if file:
+            pickle.dump(self.liste_noeuds, file)
 
-    def add_intersection_point(self, event):
+    def charger_points(self):
+        files = [('Text Document', '*.pkl')]
+        file = askopenfile(
+            filetypes=files, defaultextension=files, mode="rb")
+        if file:
+            self.liste_noeuds = pickle.load(file)
+            print(self.liste_noeuds)
+            print(self.liste_noeuds[0].id)
+            self.afficher_points()
+
+    def afficher_points(self):
+        """
+        """
+        for noeud in self.liste_noeuds:
+            (x, y) = noeud.coords
+            noeud_canvas = self.canvas.create_oval(
+                x-self.taille, y-self.taille, x+self.taille, y+self.taille, fill="red", tags='noeud')
+
+    def retour_arriere(self, event):
+        """
+        """
+        print("ctrl")
+        if self.retour_liste:
+            self.canvas.delete(self.retour_liste.pop())
+
+    def trouver_nom_noeud(self, id):
+        """
+        """
+        for key, value in enumerate(self.liste_noeuds):
+            if value.id == id:
+                print(value)
+                return value
+
+    def trouver_noeud_proche(self, event):
+
+        self.current = self.canvas.find_closest(
+            self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
+
+        tag = self.canvas.gettags(self.current)
+
+        nom = self.trouver_nom_noeud(self.current[0])
+        coords = self.canvas.coords(self.current)
+
+        if "noeud" in tag:
+            return nom, coords
+
+    def debut_ligne(self, event):
+
+        nom, coords = self.trouver_noeud_proche(event)
+        try:
+            self.noeud1 = nom
+            self.debut_x, self.debut_y = coords[0], coords[1]
+
+        except:
+            print("Pas de noeud")
+
+    def tracer_ligne(self, event):
+
+        nom, coords = self.trouver_noeud_proche(event)
+
+        try:
+
+            self.noeud2 = nom
+            self.ligne = self.canvas.create_line(
+                self.debut_x+self.taille, self.debut_y+self.taille, coords[2]-self.taille, coords[3]-self.taille)
+
+            self.canvas.itemconfig(self.ligne, width=3)
+
+            self.retour_liste.append(self.ligne)
+
+        except:
+            pass
+
+    def ajouter_arete(self, event):
+        pass
+
+    def ajouter_noeud(self, event):
         # Ajoute un point d'intersection à la liste
-        self.intersection_points.append(
-            (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)))
+        # self.liste_noeuds.append(
+        #     (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)))
 
         # Dessine un petit cercle pour représenter le point d'intersection
         x, y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
-        self.canvas.create_oval(x-10, y-10, x+10, y+10
-        
-        , fill="red")
+        noeud_canvas = self.canvas.create_oval(
+            x-self.taille, y-self.taille, x+self.taille, y+self.taille, fill="red", tags='noeud')
+
+        # Recupere ID du noeud
+        noeud_canvas_id = self.canvas.find_withtag(noeud_canvas)[0]
+
+        # Ajoute a liste_noeuds le nouveau point
+        self.liste_noeuds.append(
+            Noeud(nom=chr(63+noeud_canvas_id), voisins={}, id=noeud_canvas_id, coords=(x, y)))
+        print(f'noeud_canvas_id: {noeud_canvas_id}')
+        print(self.liste_noeuds)
 
 
 root = tk.Tk()
-app = MainApplication(master=root)
+app = MainApplication(root=root)
 app.mainloop()
