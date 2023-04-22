db8a98aa75c8697817e9409708d3c58efc840131
Elta305 <71965677+Elta305@users.noreply.github.com>
2023-04-22 21:41:43 +0200

    ajout de l'algo a*

--- graphe.py
+++ graphe.py
@@ -1,3 +1,5 @@
+import time
+
 class Noeud:
     def __init__(self, nom, voisins, id, coords, station):
         self.nom = nom
@@ -83,12 +85,11 @@ def creer_graphe():
 
 
 def dijkstra(graphe, depart, arrivee, niveau_skieur):
+    t = time.time()
+
     # Initialisation
     noeuds = graphe.get_noeuds()
-    for noeud in noeuds:
-        noeud.cout = float("inf")
-        noeud.precedent = None
-    depart.cout = 0
+    initialisation(noeuds, depart, "dijkstra")
 
     # Boucle principale
     noeuds_a_traiter = noeuds.copy()
@@ -101,54 +102,118 @@ def dijkstra(graphe, depart, arrivee, niveau_skieur):
         if niveau_skieur == "debutant":
             for voisin, (poids_voisin, _, couleur) in graphe.get_voisins(noeud).items():
                 # On calcule le cout du chemin
-                if couleur == "bleu":
-                    cout_chemin = noeud.cout + poids_voisin*2
-                elif couleur == "rouge":
-                    cout_chemin = noeud.cout + poids_voisin*5
-                elif couleur == "noir":
-                    cout_chemin = noeud.cout + poids_voisin*8
-                elif couleur == "télécabine":
-                    cout_chemin = noeud.cout + poids_voisin*3
-                elif couleur == "téléski":
-                    cout_chemin = noeud.cout + poids_voisin*4
-                elif couleur == "télésiège":
-                    cout_chemin = noeud.cout + poids_voisin*5
-                else:
-                    cout_chemin = noeud.cout + poids_voisin
+                cout_chemin = plus_court_chemin(noeud, poids_voisin, couleur, "d")
                 # On regarde si le chemin est plus court
-                if cout_chemin < voisin.cout:
-                    voisin.cout = cout_chemin
-                    voisin.precedent = noeud
+                verif_plus_court_chemin(noeud, voisin, cout_chemin, arrivee, "dijkstra")
         elif niveau_skieur == "expert":
             for voisin, (_, poids_voisin, couleur) in graphe.get_voisins(noeud).items():
                 # On calcule le cout du chemin
-                if couleur == "bleu":
-                    cout_chemin = noeud.cout + poids_voisin*1.5
-                elif couleur == "rouge":
-                    cout_chemin = noeud.cout + poids_voisin*2
-                elif couleur == "noir":
-                    cout_chemin = noeud.cout + poids_voisin*3
-                elif couleur == "télécabine":
-                    cout_chemin = noeud.cout + poids_voisin*3
-                elif couleur == "téléski":
-                    cout_chemin = noeud.cout + poids_voisin*4
-                elif couleur == "télésiège":
-                    cout_chemin = noeud.cout + poids_voisin*5
-                else:
-                    cout_chemin = noeud.cout + poids_voisin
+                cout_chemin = plus_court_chemin(noeud, poids_voisin, couleur, "d")
                 # On regarde si le chemin est plus court
-                if cout_chemin < voisin.cout:
-                    voisin.cout = cout_chemin
-                    voisin.precedent = noeud
+                verif_plus_court_chemin(noeud, voisin, cout_chemin, arrivee, "dijkstra")
 
     # On reconstruit le chemin
+    chemin = reconstruction_chemin(arrivee)
+    print("Chemin le plus court de {} à {} :".format(depart.nom, arrivee.nom))
+    print(chemin, '---------------')
+    print("Temps d'execution : {} secondes".format(time.time() - t))
+    return chemin, arrivee.cout
+
+def astar(graphe, depart, arrivee, niveau_skieur):
+    t = time.time()
+    # Initialisation
+    noeuds = graphe.get_noeuds()
+    initialisation(noeuds, depart, "astar")
+
+    # Boucle principale
+    noeuds_a_traiter = noeuds.copy()
+    while len(noeuds_a_traiter) > 0:
+        # On prend le noeud avec le cout le plus faible
+        noeud = min(noeuds_a_traiter, key=lambda x: x.cout_estime)
+        # On le retire de la liste des noeuds à traiter
+        noeuds_a_traiter.remove(noeud)
+        # On regarde les voisins du noeud
+        if niveau_skieur == "debutant":
+            for voisin, (poids_voisin, _, couleur) in graphe.get_voisins(noeud).items():
+                # On calcule le cout du chemin
+                cout_chemin = plus_court_chemin(noeud, poids_voisin, couleur, "d")
+                # On regarde si le chemin est plus court
+                verif_plus_court_chemin(noeud, voisin, cout_chemin, arrivee, "astar")
+        elif niveau_skieur == "expert":
+            for voisin, (_, poids_voisin, couleur) in graphe.get_voisins(noeud).items():
+                # On calcule le cout du chemin
+                cout_chemin = plus_court_chemin(graphe, noeud, voisin, poids_voisin, couleur, "e")
+                # On regarde si le chemin est plus court
+                verif_plus_court_chemin(noeud, voisin, cout_chemin, arrivee, "astar")
+
+    # On reconstruit le chemin
+    chemin = reconstruction_chemin(arrivee)
+    print("Chemin le plus court de {} à {} :".format(depart.nom, arrivee.nom))
+    print(chemin, '---------------')
+    print("Temps d'execution : {} secondes".format(time.time() - t))
+    return chemin, arrivee.cout
+
+
+def initialisation(noeuds, depart, algo):
+    for noeud in noeuds:
+        noeud.cout = float("inf")
+        if algo == "astar":
+            noeud.cout_estime = float("inf")
+        noeud.precedent = None
+    depart.cout = 0
+    if algo == "astar":
+        depart.cout_estime = 0
+
+def plus_court_chemin(noeud, poids_voisin, couleur, niveau_skieur):
+    if niveau_skieur == "d":
+        if couleur == "bleu":
+            cout_chemin = noeud.cout + poids_voisin*2
+        elif couleur == "rouge":
+            cout_chemin = noeud.cout + poids_voisin*5
+        elif couleur == "noir":
+            cout_chemin = noeud.cout + poids_voisin*8
+        elif couleur == "téléphérique":
+            cout_chemin = noeud.cout + poids_voisin*3
+        elif couleur == "téléski":
+            cout_chemin = noeud.cout + poids_voisin*4
+        elif couleur == "télésiège":
+            cout_chemin = noeud.cout + poids_voisin*5
+        else:
+            cout_chemin = noeud.cout + poids_voisin
+    else:
+        if couleur == "bleu":
+            cout_chemin = noeud.cout + poids_voisin*1.5
+        elif couleur == "rouge":
+            cout_chemin = noeud.cout + poids_voisin*2
+        elif couleur == "noir":
+            cout_chemin = noeud.cout + poids_voisin*3
+        elif couleur == "téléphérique":
+            cout_chemin = noeud.cout + poids_voisin*3
+        elif couleur == "téléski":
+            cout_chemin = noeud.cout + poids_voisin*4
+        elif couleur == "télésiège":
+            cout_chemin = noeud.cout + poids_voisin*5
+        else:
+            cout_chemin = noeud.cout + poids_voisin
+    return cout_chemin
+
+
+def verif_plus_court_chemin(noeud, voisin, cout_chemin, arrivee, algo):
+    if cout_chemin < voisin.cout:
+        voisin.cout = cout_chemin
+        voisin.precedent = noeud
+        if algo == "astar":
+            voisin.cout_estime = cout_chemin + abs(voisin.coords[0] - arrivee.coords[0]) + abs(voisin.coords[1] - arrivee.coords[1])
+
+
+def reconstruction_chemin(arrivee):
     chemin = []
     noeud = arrivee
     while noeud is not None:
         chemin.append(noeud)
         noeud = noeud.precedent
     chemin.reverse()
-    return chemin, arrivee.cout
+    return chemin
 
 
 def main():
@@ -156,8 +221,6 @@ def main():
     depart = graphe.get_noeud("A")
     arrivee = graphe.get_noeud("E")
     chemin = dijkstra(graphe, depart, arrivee, "debutant")
-    print("Chemin le plus court de {} à {} :".format(depart.nom, arrivee.nom))
-    print(chemin)
 
 
 if __name__ == "__main__":
